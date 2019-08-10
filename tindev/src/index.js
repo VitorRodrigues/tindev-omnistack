7b363ee7da8c925005ab36dc1bb1a70d7078aa75
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2019-08-10 17:27:05 -0300

    Add react-native app for iOS and Android

---
+++ tindev/src/index.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import Routes from './routes';
+
+// defaut way of declaring the default app function
+// const App = () => {
+//   return (
+//     <Text>Hello World!</Text>
+//   );
+// };
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
